71b2466a55d27be413768ccb1b695f772a18b299
option4futures <raju_eee243@yahoo.co.in>
2025-12-18 18:30:47 +0530

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -27,7 +27,7 @@ ist = pytz.timezone("Asia/Kolkata")
 now = datetime.now(ist)
 current_time = now.time()
 market_open = time(9, 10)
-market_close = time(15, 35)
+market_close = time(19, 35)
 
 if not (market_open <= current_time <= market_close) or now.weekday() >= 5:
     print("📉 Market is closed, exiting script.")
